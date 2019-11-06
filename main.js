b8b5a4e27159d280f84eb82d30c09e800b79437e
cipher982 <david010@gmail.com>
2019-11-06 12:11:57 -0500

    added function for rendering image in HTML and eventListeners for responding to user actions

--- main.js
+++ main.js
@@ -17,13 +17,35 @@ const predict = async (modelURL) => {
         data.append('file', img);
 
         const processedImage = await fetch("/api/prepare",
-        {
-            method: 'POST',
-            body: data
-        }).then(response => {
-            return response.json();
-        }).then(result => {
-            return tf.tensor2d(result['image']);
-        });
+            {
+                method: 'POST',
+                body: data
+            }).then(response => {
+                return response.json();
+            }).then(result => {
+                return tf.tensor2d(result['image']);
+            });
+        
+        // Shape has to be the same as in training the model
+        const prediction = model.predict(tf.reshape(processedImage, shape = [1, 28, 28, 1]));
+        const label = prediction.argMax(axis = 1).get([0]);
+        renderImageLabel(img, label);
     })
 };
+
+const renderImageLabel = (img, label) => {
+    const reader = new FileReader();
+    reader.onload = () => {
+        preview.innerHTML += `<div class="image-block">
+                                <img src="${reader.result}" class="image-block_loaded" id="source"/>
+                                 <h2 class="image-block__label">${label}</h2>
+                              </div>`;
+
+    };
+    reader.readAsDataURL(img);
+};
+
+
+fileInput.addEventListener("change", () => numberOfFiles.innerHTML = "Selected" + fileInput.files.length + " files", false);
+predictButton.addEventListener("click", () => predict(modelURL));
+clearButton.addEventListener("click", () => preview.innerHTML = "");
